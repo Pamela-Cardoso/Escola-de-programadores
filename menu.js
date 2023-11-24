29d7a1832e78c7a479e3496c61ca75b31e06bcd1
Pamela-Cardoso <145072496+Pamela-Cardoso@users.noreply.github.com>
2023-11-23 22:18:00 -0300

    Projeto Final

--- menu.js
+++ menu.js
@@ -1,10 +1,15 @@
 let btnMenu=document.getElementById('btn-menu')
 let menu= document.getElementById ('menu-mobile')
+let overlay= document.getElementById ('overlay-menu')
 
-btnMenu.addEventListener('click',()=>{
+
+btnMenu.addEventListener('click', ()=>{
     menu.classList.add('abrir-menu')
 })
 
-Menu.addEventListener('click',()=>{
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+overlay.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
 })
